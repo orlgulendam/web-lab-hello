27b0d3d115a02bb48252e118b27316ef046531a1
Gülendam Oral <orlgulendam@gmail.com>
2026-02-23 23:21:13 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+   return (
+ <div>
+ <h1>Web Tasarimi ve Programlama </h1>
+ <h2>LAB -1</h2>
+ <p>Ad Soyad: [Kendi Adin]</p>
+ <p>Ogrenci No: [Numaran]</p>
+ </div>
+ )
+}
+ export default App
